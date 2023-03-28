cbb032a672227b8d328ce8f95bd86ade8d0baee8
Milos Popovic <princpop00@gmail.com>
2023-03-28 19:07:48 +0200

    Paginacija i zavrsne kreacije

---
+++ src/components/Pagination.js
@@ -0,0 +1,23 @@
+function Pagination({ productsPerPage, totalProducts, paginate }){
+  const pageNumbers = [];
+
+  for (let i = 1; i <= Math.ceil(totalProducts / productsPerPage); i++) {
+    pageNumbers.push(i);
+  }
+
+  return (
+    <nav className='page-container'>
+      <ul className='pagination'>
+        {pageNumbers.map(number => (
+          <li key={number} className='page-item'>
+            <a onClick={() => paginate(number)} href='!#' className='page-link'>
+              {number}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+export default Pagination;
